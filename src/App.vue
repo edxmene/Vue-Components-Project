29c87fe4ac751f99405c77d907ddf4dfea3acac6
Edxon Meneses <edxon.meneses@gmail.com>
2021-10-20 09:22:37 -0500

    Adding the store resources componenet with some styling

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <stored-resources :resources="storedResources"> </stored-resources>
+</template>
+
+<script>
+import storedResources from './components/learning-resources/StoredResources.vue';
+export default {
+  components: {
+    storedResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learning how to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
